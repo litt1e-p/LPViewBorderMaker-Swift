f4df789f67c6de6fa018ad1692e1aa536df497d5
litt1e-p <free9fw@gmail.com>
2016-07-02 14:42:29 +0800

    make extension public

--- LPViewBorderMaker-Swift/UIView+Border.swift
+++ LPViewBorderMaker-Swift/UIView+Border.swift
@@ -1,27 +1,10 @@
 //
 //  UIView+Border.swift
-//  LPViewBorderMaker-Swift
+//  LPViewBorderMaker-Swift-Sample
 //
 //  Created by litt1e-p on 16/6/30.
-//  Copyright © 2016年 litt1e-p.(https://github.com/litt1e-p) All rights reserved.
+//  Copyright © 2016年 litt1e-p. All rights reserved.
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
 
 import UIKit
 
@@ -35,7 +18,7 @@ public struct LPBorderPosition: OptionSetType
     public init(rawValue: Int) { self.rawValue = rawValue }
 }
 
-extension UIView
+public extension UIView
 {
     public func makeBorders() {
         makeBorder(1.0, color: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5), positions: [.Top, .Bottom, .Left, .Right])
